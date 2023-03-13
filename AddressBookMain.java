00b6460b829d3211a25ebaf482c30fc913ddc0b0
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-13 17:40:23 +0530

    Added:code to add a new contact to address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,8 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Contact person = new Contact();
+        System.out.println("Enter Details:");
+        person.getPersonDetails();
     }
 }
