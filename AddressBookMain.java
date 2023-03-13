f4aa52039a90d639ac645db4aeb9a71597f89ba0
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-13 19:21:07 +0530

    Added:code to delete a person using person name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,19 +6,22 @@ public class AddressBookMain {
     public static final int ADD = 1;
     public static final int EDIT = 2;
     public static final int DISPLAY = 3;
-    public static final int EXIT = 4;
+    public static final int DELETE = 4;
+    public static final int EXIT = 5;
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
         ArrayList<AddressBook> book = new ArrayList<>();
         Scanner input = new Scanner(System.in);
-        int option= 0;
-        while (option != 4) {
+        int option = 0;
+        while (option != 5) {
             System.out.println("Options are : ");
             System.out.print("1.Add\t");
             System.out.print("2.Edit\t");
             System.out.print("3.Display\t");
-            System.out.print("4.Exit\n");
+            System.out.print("4.Delete\t");
+            System.out.print("5.Exit\n");
             System.out.print("Enter an option from above :- ");
             option = input.nextInt();
             AddressBook addressBook = new AddressBook();
@@ -37,12 +40,21 @@ public class AddressBookMain {
                     if (name.equals(AddressBook.person.getFirstName())) {
                         addressBook.editPersonDetails();
                         System.out.println(book);
-                    }
-                    else System.out.println("Contact not found");
+                    } else System.out.println("Contact not found");
                     break;
                 case DISPLAY:
                     System.out.println("Displaying Array : ");
                     System.out.println(book);
+                    break;
+                case DELETE:
+                    System.out.println("Enter a name");
+                    String name1 = input.next();
+                    for (int i = 0; i<= book.size();i++) {
+                        if (name1.equals(AddressBook.person.getFirstName())) {
+                            book.remove(i);
+                        }
+                        System.out.println(book);
+                    }
                     break;
                 case EXIT:
                     System.out.println("Exiting from book");
